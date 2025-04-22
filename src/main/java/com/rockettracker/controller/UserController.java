48cad5d0a50f620bbdb502b521d4aeb42099c420
Daniel <danielcoblentz916@gmail.com>
2025-04-22 19:22:48 -0400

    finished making endpoints for users to signup/login

---
+++ src/main/java/com/rockettracker/controller/UserController.java
@@ -0,0 +1,35 @@
+package com.rockettracker.controller;
+
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.*;
+
+import com.rockettracker.model.User;
+import com.rockettracker.service.UserService;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("/me")
+    public ResponseEntity<User> authenticatedUser() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        User currentUser = (User) authentication.getPrincipal();  // cast correctly
+        return ResponseEntity.ok(currentUser);
+    }
+
+    @GetMapping("/")
+    public ResponseEntity<List<User>> allUsers() {
+        List<User> users = userService.allUsers();
+        return ResponseEntity.ok(users);
+    }
+}
